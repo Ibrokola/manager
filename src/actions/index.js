e9baecf1dfd4260a408505ed49f5dbc6b28858c2
ibrobabs <babskolawole@gmail.com>
2017-08-20 06:48:18 -0600

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,23 @@
-import { EMAIL_CHANGED } from './types';
+import { 
+    EMAIL_CHANGED ,
+    PASSWORD_CHNAGED
+} from './types';
 
 export const emailChanged = (text) => {
     return{
         type: EMAIL_CHANGED,
         payload: text
     };
+};
+
+
+export const passwordChanged  = (text) => {
+    return {
+        type: PASSWORD_CHNAGED,
+        payload: text
+    };
+}; 
+
+export const loginUser = ({ email, password }) => {
+    
 };
