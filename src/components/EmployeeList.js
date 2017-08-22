70c3a197092f3343caf4547f24123457d4f3637f
ibrobabs <babskolawole@gmail.com>
2017-08-22 10:38:30 -0600

    update

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,32 +1,54 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
 import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
  
 class EmployeeList extends Component{
     componentWillMount() {
         this.props.employeesFetch();
 
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        //  nextProps are the next set of props that this componenet wil be rendered with
+        // this.props is still the old set of props
+
+        this.createDataSource(nextProps);
+
+    }
+
+    createDataSource({ employees }) {
         const ds = new ListView.DataSoource({
             rowHasChanged: (r1, r2) => r1 != r2
         });
 
-        this .dataSoource = ds.cloneWithROws(this.props.employees);
+        this .dataSource = ds.cloneWithRows(employees);
+    }
+
+    renderRow(employe) {
+        return <ListItem employee={employee} />;
     }
 
     render() {
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <ListView 
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         );
     }
 }
 
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => { 
+        return { ...val, uid };
+    });
 
-export default connect(null, { employeesFetch }) (EmployeeList);
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch }) (EmployeeList);
